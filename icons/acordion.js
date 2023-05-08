b5807b855b60f4e9e2c386e58d04a48e00c659e6
aginga <brianaginga35@gmail.com>
2023-05-08 12:42:46 -0700

    how to do it

---
+++ icons/acordion.js
@@ -0,0 +1,17 @@
+let acc = document.getElementsByClassName("acordion");
+
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+
+        this.classList.toggle("active")
+
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none"
+        }else{
+            panel.style.display = "block"
+        }
+    });
+}
